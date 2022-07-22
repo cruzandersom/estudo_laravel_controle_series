b8803407a1982de47a7d12c2c2bca2640fc90a9d
Anderson Cruz <cruz.andersom@gmail.com>
2022-07-22 16:28:47 -0300

    Inicio_das_atividades_quinta_2107 Fim_das_atividades_2107 Inicio_atividades_sexta_2207

---
+++ app/Http/Requests/SeriesFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SeriesFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        /// como não há necessidade AINDA  o request será deixado como true
+        /// assim todo mundo poderá fazer request
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nome' => ['required','min:3', 'max:255']
+        ];
+    }
+}
