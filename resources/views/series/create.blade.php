75c5ec886f8c507c34787797d5ff818b959bd7c1
Anderson Cruz <cruz.andersom@gmail.com>
2022-07-23 11:19:27 -0300

    Inicio_das_atividades_quinta_2107 Fim_das_atividades_2107 Inicio_atividades_sexta_2207 Fim_atividades_sexta_2207 fim_atividade_sabado_2307

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,41 @@
 <x-layout title="Nova Serie">
 
-    <x-series.form :action="route('series.store')" :nome="old('nome')" :update="false"/>
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+
+        <div class="row mb-3">
+            <div class="col-8">
+                <label for="nome" class="form-label">Nome:</label>
+                <input type="text"
+                       id="nome"
+                       name="nome"
+                       class="form-control"
+                       value="{{ old('nome') }}">
+            </div>
+
+            <div class="col-2">
+                <label for="nome" class="form-label">T:</label>
+                <input type="text"
+                       id="nome"
+                       name="nome"
+                       class="form-control"
+                       value="{{ old('nome') }}">
+            </div>
+
+            <div class="col-2">
+                <label for="nome" class="form-label">Nome:</label>
+                <input type="text"
+                       id="nome"
+                       name="nome"
+                       class="form-control"
+                       value="{{ old('nome') }}">
+            </div>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+
+
 
 </x-layout>
